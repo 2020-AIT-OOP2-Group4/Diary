d75eb0bfa3939dd3db002549823f431463aabd26
takahiro75 <75057593+takahiro75@users.noreply.github.com>
2020-11-29 22:26:26 +0900

    俺の日記
    My Diary

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from diaries.takamotoDiaryNew import TakamotoDiaryNew
 from diaries.k19056DiaryNew import k19056DiaryNew
 from diaries.K19096DiaryNew import K19096DiaryNew
 from diaries.k19071DiaryNew import k19071DiaryNew
+from diaries.K19101DiaryNew import K19101DiaryNew 
 
 
-diaries = [DiarySample(), k19056DiaryNew(), TakamotoDiaryNew(), K19096DiaryNew(), k19071DiaryNew(),]
+diaries = [DiarySample(), k19056DiaryNew(), TakamotoDiaryNew(), K19096DiaryNew(), k19071DiaryNew(),K19101DiaryNew(),]
 for d in diaries:
     print("---------------------------------")
     print(d.get_date())
